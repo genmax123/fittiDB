1e0f401fcd2a5cc25853f14a37176d1ae48eca11
User <User@NB-Max-02.max.local>
2025-11-13 17:25:19 +0100

    hello from NB-MAX-02

--- src/fittidb/FittiDB.java
+++ src/fittidb/FittiDB.java
@@ -16,6 +16,7 @@ public class FittiDB {
     public static void main(String[] args) {
         // TODO code application logic here
     System.out.println("Hallo Welt");
+        System.out.println("Hallo from NB-Max-02");
     }
     
 }
